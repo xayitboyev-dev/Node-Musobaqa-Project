a8cff1c931ef10a3ba4ec1556774da1bdd68160c
BoburAbdullayev <changeitgo3@gmail.com>
2023-02-18 10:36:29 +0500

    feat: mongo

---
+++ utils/mongo.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const connectToMongoDB = async (url) => {
+    try {
+        mongoose.set('strictQuery', true);
+        await mongoose.connect(url, () => {
+            console.log('MongoDB connected successfully');
+        })
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+exports = connectToMongoDB
